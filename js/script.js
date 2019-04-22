8967e99cf67f324034306d63f7821f32a0c99034
Safinah Ali <aied-staging@gmail.com>
2019-04-21 22:31:56 -0400

    resources tabs

--- js/script.js
+++ js/script.js
@@ -390,6 +390,23 @@ jQuery(function ($) {
         }
     });
 
+    $("#price-slider-curricula").owlCarousel({
+        items: 3,
+        dots: false,
+        nav: false,
+        responsive: {
+            991: {
+                items: 3,
+            },
+            767: {
+                items: 2,
+            },
+            320: {
+                items: 1,
+            },
+        }
+    });
+
     $("#team-three-slider").owlCarousel({
         items: 4,
         dots: false,
