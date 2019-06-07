d5a4e0ba0a6473df7f48dd9bbdbadb1b96374449
safinahali <safinahaali@gmail.com>
2019-06-07 18:27:46 -0400

    switched banner4

--- js/script.js
+++ js/script.js
@@ -390,6 +390,23 @@ jQuery(function ($) {
         }
     });
 
+    $("#price-slider-1").owlCarousel({
+        items: 3,
+        dots: false,
+        nav: false,
+        responsive: {
+            991: {
+                items: 3,
+            },
+            767: {
+                items: 2,
+            },
+            320: {
+                items: 1,
+            },
+        }
+    });
+
     $("#price-slider-2").owlCarousel({
         items: 3,
         dots: false,
