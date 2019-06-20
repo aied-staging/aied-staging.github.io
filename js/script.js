ae78e46a317fc6fca020b378dade6ee9b3e725f1
malalawi1 <Hinamorimomo1>
2019-06-20 16:18:55 -0400

    Logosand Partners added; index.html updates with partners section

--- js/script.js
+++ js/script.js
@@ -508,6 +508,23 @@ jQuery(function ($) {
             },
         }
     });
+    
+    $("#team-three-slider-2").owlCarousel({
+        items: 4,
+        dots: false,
+        nav: false,
+        responsive: {
+            991: {
+                items: 4,
+            },
+            767: {
+                items: 2,
+            },
+            320: {
+                items: 1,
+            },
+        }
+    });
 
     //testimonial slider
 
