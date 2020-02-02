eb3e9d4ceff5263467b53e5fb594dd160ba4df1b
safinahali <safinahaali@gmail.com>
2020-02-02 16:44:38 -0500

    changed to home and projects

--- js/script.js
+++ js/script.js
@@ -460,6 +460,26 @@ jQuery(function ($) {
         }
     });
 
+
+    $("#price-slider-5").owlCarousel({
+        items: 3,
+        dots: false,
+        nav: false,
+        responsive: {
+            991: {
+                items: 3,
+            },
+            767: {
+                items: 2,
+            },
+            320: {
+                items: 1,
+            },
+        }
+    });
+
+
+
     $("#price-slider-curricula").owlCarousel({
         items: 3,
         dots: false,
